ac78c6e2e297617f31f87650476b88ecc16de023
Frostre_ <n9043395@gmail.com>
2026-09-15 06:02:09 +0200

    Add doFork function in fork.zig
    Implement doFork function for process forking based on architecture.

---
+++ src/exec/fork.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const linux = std.os.linux;
+const arch = builtin.cpu.arch;
+const types = @import("../common/types.zig");
+const sig = @import("../signal/numbers.zig");
+
+pub fn doFork() !types.pid_t {
+    const result = if (arch == .x86_64) blk: {
+        break :blk linux.syscall0(.fork);
+    } else if (arch == .aarch64) blk: {
+        break :blk linux.syscall5(.clone, sig.SIGCHLD_FLAG, 0, 0, 0, 0);
+    } else @compileError("unsupported arch");
+
+    const signed: isize = @bitCast(result);
+    if (signed < 0) return error.ForkFailed;
+    return @intCast(signed);
+}
